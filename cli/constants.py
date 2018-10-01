531211276e1b101e1f989410bab51f57c1b885a1
Diana Lucaci <ldiana@google.com>
2018-10-01 16:13:32 +0100

    Initial version of CLI including Cron options

---
+++ cli/constants.py
@@ -0,0 +1,57 @@
+# Copyright 2018 Google Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+PROJECT_DIR = os.path.join(os.path.dirname(__file__), '..')
+SCRIPTS_DIR = "{}/scripts".format(PROJECT_DIR)
+STAGE_DIR = "{}/scripts/variables/stages".format(PROJECT_DIR)
+CRON_FILE = "{}/backends/cron.yaml".format(PROJECT_DIR)
+
+CRON_TEMPLATE = """# Copyright 2018 Google Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cron:
+- description: cron service handler
+  url: /cron
+  schedule: every {} {}
+  target: job-service
+"""
+
+EMPTY_CRON_TEMPLATE = """# Copyright 2018 Google Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+cron:
+"""
