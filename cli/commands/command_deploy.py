11041c7e5d60dba3a7a712bffabd72174c7bc560
Diana Lucaci <ldiana@google.com>
2018-10-03 12:37:32 +0100

    Changed indentation

--- cli/commands/command_deploy.py
+++ cli/commands/command_deploy.py
@@ -16,15 +16,14 @@ import os
 import click
 import constants
 
-
 def _get_stage_file(stage):
-    stage_file = "{}/{}.sh".format(constants.STAGE_DIR, stage)
-    return stage_file
+  stage_file = "{}/{}.sh".format(constants.STAGE_DIR, stage)
+  return stage_file
 
 @click.group()
 def cli():
-    """Deploy cli"""
-    pass
+  """Deploy cli"""
+  pass
 
 # [TODO] Make cm and ch options mutual exclusiv
 @cli.command('cron')
@@ -34,29 +33,29 @@ def cli():
 @click.option('--cron-frequency-hours', '-ch', default=None, show_default=True,
               help='Cron job schedule in hours')
 def cron(stage, cron_frequency_minutes, cron_frequency_hours):
-    """Deploy only the cron file for a STAGE"""
-    stage_file = _get_stage_file(stage)
-    if not os.path.isfile(stage_file):
-        click.echo("Stage file not found.")
-        exit(1)
-    else:
-        with open(constants.CRON_FILE, "w") as cron_file:
-            if cron_frequency_minutes is None and cron_frequency_hours is None:
-                cron_file.write(constants.EMPTY_CRON_TEMPLATE)
-            else:
-                if cron_frequency_minutes:
-                    cron_file.write(constants.CRON_TEMPLATE
-                                    .format(str(cron_frequency_minutes),
-                                            "minutes"))
-                if cron_frequency_hours:
-                    cron_file.write(constants.CRON_TEMPLATE
-                                    .format(str(cron_frequency_hours),
-                                            "hours"))
-        os.system("""source \"{}\"
-                source \"{}/deploy/before_hook.sh\"
-                source \"{}/deploy/cron.sh\""""
-                  .format(stage_file, constants.SCRIPTS_DIR, constants.SCRIPTS_DIR))
-
+  """Deploy only the cron file for a STAGE"""
+  stage_file = _get_stage_file(stage)
+  if not os.path.isfile(stage_file):
+      click.echo("Stage file not found.")
+      exit(1)
+  else:
+      with open(constants.CRON_FILE, "w") as cron_file:
+          if cron_frequency_minutes is None and cron_frequency_hours is None:
+              cron_file.write(constants.EMPTY_CRON_TEMPLATE)
+          else:
+              if cron_frequency_minutes:
+                  cron_file.write(constants.CRON_TEMPLATE
+                                  .format(str(cron_frequency_minutes),
+                                          "minutes"))
+              if cron_frequency_hours:
+                  cron_file.write(constants.CRON_TEMPLATE
+                                  .format(str(cron_frequency_hours),
+                                          "hours"))
+      os.system("""source \"{}\"
+              source \"{}/deploy/before_hook.sh\"
+              source \"{}/deploy/cron.sh\""""
+                .format(stage_file, constants.SCRIPTS_DIR,
+                        constants.SCRIPTS_DIR))
 
 if __name__ == '__main__':
-    cli()
+  cli()
