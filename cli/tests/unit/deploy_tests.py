8b02fff4c4ce3ce18e02174625230a274f2a338a
Diana Lucaci <ldiana@google.com>
2018-10-15 14:23:57 +0100

    add ibackend unit test

--- cli/tests/unit/deploy_tests.py
+++ cli/tests/unit/deploy_tests.py
@@ -32,17 +32,38 @@ class TestDeploy(TestCase):
   def tearDown(self):
     super(TestDeploy, self).tearDown()
 
+  @staticmethod
+  def _get_mocked_stage(mocked_stage_name, mocked_path,
+                        stage_example_path=crmint_commands._constants.STAGE_EXAMPLE_PATH):
+    copyfile(stage_example_path, "{}.py".format(mocked_stage_name))
+    mocked_stage = imp.load_source(mocked_stage_name,
+                                   os.path.join(os.getcwd(),
+                                                "{}.py".format(mocked_stage_name)))
+    mocked_stage.workdir = mocked_path
+    return mocked_stage
+
   @mock.patch('crmint_commands.deploy._check_stage_file')
   @mock.patch('crmint_commands.deploy._get_stage_object')
   def test_frontend_succeeded(self, mocked_get_stage_object, mocked_check_stage_file):
+    mocked_stage_name = "mocked_stage"
     mocked_check_stage_file.return_value = True
     runner = CliRunner()
     with runner.isolated_filesystem():
-      copyfile(crmint_commands.deploy._constants.STAGE_EXAMPLE_PATH, "mocked_stage_file.py")
-      mocked_stage = imp.load_source("mocked_stage_file", os.path.join(os.getcwd(), "mocked_stage_file.py"))
-      mocked_stage.workdir = os.getcwd()
-      mocked_get_stage_object.return_value = mocked_stage
-    result = runner.invoke(crmint_commands.deploy.frontend, ['mocked_stage_name'])
+      mocked_get_stage_object.return_value = TestDeploy._get_mocked_stage(mocked_stage_name,
+                                                                          os.getcwd())
+    result = runner.invoke(crmint_commands.deploy.frontend, [mocked_stage_name])
+    self.assertEqual(result.exit_code, 0)
+
+  @mock.patch('crmint_commands.deploy._check_stage_file')
+  @mock.patch('crmint_commands.deploy._get_stage_object')
+  def test_ibackend_succeeded(self, mocked_get_stage_object, mocked_check_stage_file):
+    mocked_stage_name = "mocked_stage"
+    mocked_check_stage_file.return_value = True
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+      mocked_get_stage_object.return_value = TestDeploy._get_mocked_stage(mocked_stage_name,
+                                                                          os.getcwd())
+    result = runner.invoke(crmint_commands.deploy.ibackend, [mocked_stage_name])
     self.assertEqual(result.exit_code, 0)
 
   def test_cron_stage_not_found(self):
