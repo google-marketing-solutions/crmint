0f8a33570ea2795107cac57d74723194d68089c0
Diana Lucaci <ldiana@google.com>
2018-10-19 18:05:12 +0100

    dev setup

--- cli/crmint_commands/dev.py
+++ cli/crmint_commands/dev.py
@@ -14,18 +14,68 @@
 
 import click
 from crmint_commands.utils import database
+from crmint_commands.utils import constants
+import os
+import subprocess
+
 
 @click.group()
 def cli():
   """CRMint Local Dev CLI"""
   pass
 
+####################### SETUP #######################
+
+CONFIG_FILES = [
+    ("backends/instance/config.py.example", "backends/instance/config.py"),
+    ("backends/gae_dev_ibackend.yaml.example", "backends/gae_dev_ibackend.yaml"),
+    ("backends/gae_dev_jbackend.yaml.example", "backends/gae_dev_jbackend.yaml"),
+    ("backends/data/app.json.example", "backends/data/app.json"),
+    ("backends/data/service-account.json.example", "backends/data/service-account.json")
+]
+
+REQUIREMENTS_DIR = os.path.join(constants.PROJECT_DIR, "cli/requirements.txt")
+LIB_DEV_PATH = os.path.join(constants.PROJECT_DIR, "backends/lib_dev")
+
+
+def _create_config_file(example_path, dest):
+  if not os.path.exists(dest):
+    copyfile(example_path, dest)
+
+
+def _create_all_configs():
+  for config in CONFIG_FILES:
+    full_src_path = os.path.join(constants.PROJECT_DIR, config[0])
+    full_dest_path = os.path.join(constants.PROJECT_DIR, config[1])
+    _create_config_file(full_src_path, full_dest_path)
+
+
+def _pip_install():
+  try:
+    resp = subprocess.Popen("pip install -r {} -t {}".format(REQUIREMENTS_DIR, LIB_DEV_PATH),
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     shell=True)
+  except:
+    raise Exception("Requirements could not be installed")
+
 
 @cli.command('setup')
 def setup():
-  """Setup DB and config files required for local development."""
-  database.create_database()
-  pass
+  """Setup DB and config files required for local development."""  
+  click.echo("Setup in progress...")
+  try:
+    components = [database.create_database, _create_all_configs, _pip_install]
+    with click.progressbar(components) as progress_bar:
+      for component in progress_bar:
+        component()
+  except Exception as exception:
+    click.echo("Setup failed: {}".format(exception))
+    exit(1)
+
+
+####################### RUN #######################
+
 
 @cli.command('run')
 @click.argument('component')
