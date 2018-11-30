0270364b7bd7e2a64dee47d673051e612f0492e0
Pierre Dulac <dulacp@google.com>
2018-11-30 17:07:35 +0100

    Improve the cli setup script to work from GCP cloud shell

--- cli/setup.py
+++ cli/setup.py
@@ -12,15 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="crmint",
-    version='0.1',
-    py_modules=['appcli'],
-    entry_points='''
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
         [console_scripts]
-        crmint=cli.appcli:entry_point
-    ''',
+        crmint=appcli:entry_point
+    """,
 )
