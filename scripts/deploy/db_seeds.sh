a61bcdb144dbe336353464344b9faf3607272e7f
Pierre Dulac <dulacpier@gmail.com>
2018-12-03 18:31:46 +0100

    Remove the outdated bash scripts :boom:

--- scripts/deploy/db_seeds.sh
+++
@@ -1,8 +0,0 @@
-source "$SCRIPTS_DIR/variables/common.sh"
-source "$SCRIPTS_DIR/deploy/before_task.sh"
-
-cp "$SCRIPTS_DIR/deploy/tasks/seeds.py" "$workdir/backends/"
-task_path="$workdir/backends/seeds.py"
-python $task_path
-source "$SCRIPTS_DIR/deploy/after_task.sh"
-rm -f $task_path
