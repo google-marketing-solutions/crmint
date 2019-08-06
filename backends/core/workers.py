c5512825b16f61543163bfd4af53c239538987a5
Manjooth Kler <manjoothkler@gmail.com>
2019-08-06 15:28:18 +0100

    Tidied up code

--- backends/core/workers.py
+++ backends/core/workers.py
@@ -14,32 +14,31 @@
 
 """Module with CRMintApp worker classes."""
 
-
-from datetime import datetime
-from datetime import timedelta
-from fnmatch import fnmatch
-from functools import wraps
-import json
-import os
-from random import random
-import time
-import urllib
-import uuid
-import yaml
-import hashlib
-
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from apiclient.http import MediaIoBaseUpload
 import cloudstorage as gcs
+from datetime import datetime
+from datetime import timedelta
+from fnmatch import fnmatch
+from functools import wraps
+from googleads import adwords, common, oauth2
+import hashlib
+import json
+import logging
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+from random import random
 import requests
+import time
+import urllib
+import uuid
+import yaml
+
 from google.cloud import bigquery
 from google.cloud.exceptions import ClientError
-# Import appropriate modules from the client library.
-from googleads import adwords, common
-#import googleads
 
+logging.disable(logging.DEBUG)
 
 _KEY_FILE = os.path.join(os.path.dirname(__file__), '..', 'data',
                          'service-account.json')
@@ -56,7 +55,6 @@ AVAILABLE = (
     'StorageCleaner',
     'StorageToBQImporter',
     'AddClientMatchLists',
-    'RemoveClientMatchLists',
 )
 
 # Defines how many times to retry on failure, default to 5 times.
@@ -81,7 +79,7 @@ class Worker(object):
   PARAMS = []
 
   # Maximum number of execution attempts.
-  MAX_ATTEMPTS = 3
+  MAX_ATTEMPTS = 1
 
   def __init__(self, params, pipeline_id, job_id):
     self._pipeline_id = pipeline_id
@@ -972,7 +970,17 @@ class BQMLTrainer(BQWorker):
     job = client.run_async_query(job_name, self._params['query'])
     job.use_legacy_sql = False
     self._begin_and_wait(job)
+
+
 class AddClientMatchLists(Worker):
+
+  PARAMS = [
+      ('developer_token', 'string', True, '', 'Developer Token'),
+      ('client_id', 'string', True, '', 'Client ID'),
+      ('client_secret', 'string', True, '', 'Client Secret'),
+      ('refresh_token', 'string', True, '', 'Refresh Token'),
+      ('client_customer_id', 'string', True, '', 'Client Customer ID')
+  ]
 
   def _execute(self):
     # with open('data/googleads.yaml', 'r') as yaml_file:
@@ -983,8 +991,17 @@ class AddClientMatchLists(Worker):
     #     cache=suds.cache.NoCache())
     # adwords_client = adwords.AdWordsClient.LoadFromString(adwords_yaml)
     print 'STARTING1'
-    adwords_client = adwords.AdWordsClient.LoadFromStorage(path='data/googleads.yaml')
-    adwords_client.cache = common.ZeepServiceProxy.NO_CACHE
+    client_id = self._params['client_id']
+    client_secret = self._params['client_secret']
+    refresh_token = self._params['refresh_token']
+    dev_token = self._params['developer_token']
+    client_customer_id = self._params['client_customer_id']
+    # adwords_client = adwords.AdWordsClient.
+    # LoadFromStorage(path='data/googleads.yaml')
+    # adwords_client.cache = common.ZeepServiceProxy.NO_CACHE
+    client = AdwordsClient()
+    adwords_client = client.getClient(client_id, client_secret, refresh_token,
+                                      dev_token, client_customer_id)
     self.run(adwords_client)
 
   def run(self, client):
@@ -995,9 +1012,10 @@ class AddClientMatchLists(Worker):
     print 'STARTING3'
     user_list = {
         'xsi_type': 'CrmBasedUserList',
-        'name': 'Customer relationship management list #%d' % uuid.uuid4(),
-        'description': 'A list of customers that originated from email addresses',
-        # CRM-based user lists can use a membershipLifeSpan of 10000 to indicate
+        'name': 'Customer rel. management list #%d' % uuid.uuid4(),
+        'description': 'List of customers that originated from email addresses',
+        # CRM-based user lists can use a membershipLifeSpan of 10000
+        # to indicate
         # unlimited; otherwise normal values apply.
         'membershipLifeSpan': 30,
         'uploadKeyType': 'CONTACT_INFO'
@@ -1015,7 +1033,8 @@ class AddClientMatchLists(Worker):
     print 'STARTING6'
     emails = ['customer1@example.com', 'customer2@example.com',
               ' Customer3@example.com ']
-    members = [{'hashedEmail': self.NormalizeAndSHA256(email)} for email in emails]
+    members = [{'hashedEmail': self.NormalizeAndSHA256(email)} for
+               email in emails]
 
     # Add address info.
     members.append({
@@ -1039,7 +1058,7 @@ class AddClientMatchLists(Worker):
 
     response = user_list_service.mutateMembers([mutate_members_operation])
 
-    print 'STARTING3'
+    print 'STARTING7'
 
     if 'userLists' in response:
       for user_list in response['userLists']:
@@ -1047,13 +1066,14 @@ class AddClientMatchLists(Worker):
                % (user_list['name'], user_list['id']))
 
   def NormalizeAndSHA256(self, s):
-    """Normalizes (lowercase, remove whitespace) and hashes a string with SHA-256.
+    """Normalizes (lowercase, remove whitespace) and hashes a string
+        with SHA-256.
     Args:
       s: The string to perform this operation on.
     Returns:
       A normalized and SHA-256 hashed string.
     """
-    print 'STARTIN4'
+    print 'STARTING4'
     return hashlib.sha256(s.strip().lower()).hexdigest()
 
 
@@ -1067,3 +1087,16 @@ class RemoveClientMatchLists(Worker):
     print('Remove')
     testing_value = self._params['test']
     print(testing_value)
+
+
+class AdwordsClient():
+
+  def getClient(self, client_id, client_secret, refresh_token,
+                dev_token, client_customer_id):
+    oauth_client = oauth2.GoogleRefreshTokenClient(client_id,
+                                                   client_secret, refresh_token)
+    adwords_client = adwords.AdWordsClient(dev_token, oauth_client,
+                                           client_customer_id)
+    adwords_client.cache = common.ZeepServiceProxy.NO_CACHE
+
+    return adwords_client
