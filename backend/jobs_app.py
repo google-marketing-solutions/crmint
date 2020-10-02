04ae7174e3a50eeae6fdea953e13e8de3bfdb10b
Alexander Prikhodko <aprikhodko@google.com>
2020-10-02 19:40:48 +0100

    PubSub instead of App Engine Task Queue

--- backend/jobs_app.py
+++ backend/jobs_app.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google Inc
+# Copyright 2020 Google Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,11 @@
 # limitations under the License.
 
 
-from flask import Flask, json
+from flask import Flask, json, request
 from common import auth_filter
+from common.message import BadRequestError, TooEarlyError
+from common.task import Task
+from common.result import Result
 from jobs import workers
 
 
@@ -28,11 +31,39 @@ def workers_list():
 
 
 @app.route('/api/workers/<worker_class>/params', methods=['GET'])
-def worker_params(worker_class):
+def worker_parameters(worker_class):
   klass = getattr(workers, worker_class)
   keys = ['name', 'type', 'required', 'default', 'label']
   return (json.jsonify([dict(zip(keys, param)) for param in klass.PARAMS]),
           {'Access-Control-Allow-Origin': '*'})
+
+
+@app.route('/push/start-task', methods=['POST'])
+def start_task():
+  try:
+    task = Task.from_request(request)
+  except (BadRequestError, TooEarlyError) as e:
+    return e.message, e.code
+
+  worker_class = getattr(workers, task.worker_class)
+  worker_params = task.worker_params.copy()
+  for setting in worker_class.GLOBAL_SETTINGS:
+    worker_params[setting] = task.general_settings[setting]
+  worker = worker_class(worker_params, task.pipeline_id, task.job_id)
+
+  try:
+    workers_to_enqueue = worker.execute()
+  except workers.WorkerException as e:
+    worker.log_error('Execution failed: %s: %s', e.__class__.__name__, e)
+    result = Result(task.name, task.job_id, False)
+    result.report()
+  except Exception as e:  # pylint: disable=broad-except
+    worker.log_error('Unexpected error: %s: %s', e.__class__.__name__, e)
+    task.reenqueue()
+  else:
+    result = Result(task.name, task.job_id, True, workers_to_enqueue)
+    result.report()
+  return 'OK', 200
 
 
 if __name__ == '__main__':
