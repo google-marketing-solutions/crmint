3dc02f7bf5378abd5d9e94216a0bc233216018c6
Tyrone Schiff <tyroneschiff@gmail.com>
2022-03-11 17:27:24 -0500

    Add unique id to bq job id

--- backend/jobs/workers/bigquery/bq_worker.py
+++ backend/jobs/workers/bigquery/bq_worker.py
@@ -16,6 +16,7 @@
 
 
 import time
+import uuid
 from google.cloud import bigquery
 from jobs.workers.worker import Worker, WorkerException
 
@@ -32,24 +33,22 @@ class BQWorker(Worker):
   def _get_client(self):
     return bigquery.Client(client_options={'scopes': self._SCOPES})
 
-  def _get_prefix(self):
-    return f'{self._pipeline_id}_{self._job_id}_{self.__class__.__name__}'
+  def _get_job_id(self):
+    unique_id = str(uuid.uuid4()).replace('-', '_')
+    return f'{self._pipeline_id}_{self._job_id}_{self.__class__.__name__}_{unique_id}'
 
   def _wait(self, job):
     """Waits for job completion and relays to BQWaiter if it takes too long."""
     delay = 5
-    waiting_time = 5
     time.sleep(delay)
-    while not job.done():
-      if waiting_time > 300:  # Once 5 minutes have passed, spawn BQWaiter.
-        self._enqueue('BQWaiter', {'job_id': job.job_id}, 60)
-        return
-      if delay < 30:
-        delay = [5, 10, 15, 20, 30][int(waiting_time / 60)]
-      time.sleep(delay)
-      waiting_time += delay
+    job.reload()
+    while job.state != 'DONE':
+      self._enqueue('BQWaiter', {'bq_job_id': job.job_id}, 60)
+      return
     if job.error_result is not None:
       raise WorkerException(job.error_result['message'])
+    if job.state == 'DONE':
+      self.log_info('Finished successfully.')
 
   def _get_full_table_name(self):
     project = self._params['bq_project_id'].strip()
@@ -58,4 +57,3 @@ class BQWorker(Worker):
     dataset = self._params['bq_dataset_id'].strip()
     table = self._params['bq_table_id'].strip()
     return f'{project}{dataset}.{table}'
-
