3686d51ae51db47aaa2a518d190834671925d96a
Alexander Prikhodko <aprikhodko@google.com>
2022-06-24 22:53:55 +0100

    Fix instance naming

--- backend/controller_app.py
+++ backend/controller_app.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Create an IBackend instance."""
+"""Create a controller instance."""
 
 import signal
 import sys
