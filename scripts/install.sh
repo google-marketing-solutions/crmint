8be8acb3454ecaff7e7d829a13ed7de06919701b
Pierre Dulac <dulacp@google.com>
2023-01-13 16:05:21 +0100

    Escape backquotes to avoid the shell to execute these statements

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,7 @@ echo -e "\nAdding a bash function to your $HOME/.bashrc file."
 cat <<EOF >$HOME/.crmint
 # CRMint wrapper function.
 function crmint {
-  # CloudShell stores gcloud config in a tmp directory at `\$CLOUDSDK_CONFIG`.
+  # CloudShell stores gcloud config in a tmp directory at \`\$CLOUDSDK_CONFIG\`.
   # But to also work on local environments we default to the user home config.
   GCLOUD_CONFIG_PATH="\${CLOUDSDK_CONFIG:-\$HOME/.config/gcloud}"
   echo "Using gcloud config: \$GCLOUD_CONFIG_PATH"
