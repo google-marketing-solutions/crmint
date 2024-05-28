82aba0e2b7dc031b5b6bdca97298145198402c01
Robert McMahan <robertmcmahan@google.com>
2024-05-28 22:20:02 +0000

    Updates variable list when creating/editing model to use a dropdown instead of a list of all variables.

--- frontend/src/app/ml-models/ml-model-form/ml-model-form.component.ts
+++ frontend/src/app/ml-models/ml-model-form/ml-model-form.component.ts
@@ -190,7 +190,8 @@ export class MlModelFormComponent implements OnInit {
    */
   value(control: string|AbstractControl, key: string = ''): any {
     const formControl = control instanceof AbstractControl ? control : this.mlModelForm.get(control);
-    return key ? formControl.get(key).value : formControl.value;
+    const targetControl = key ? formControl.get(key) : formControl;
+    return targetControl ? targetControl.value : null;
   }
 
   /**
@@ -361,72 +362,96 @@ export class MlModelFormComponent implements OnInit {
     const formVariables: Variable[] = this.value('variables');
     const existingVariables: Variable[] = (formVariables.length ? formVariables : this.mlModel.variables) || [];
     const variables: Variable[] = this.getVariables();
+    const variableSources: string[] = variables.map(v => v.source).filter((s, i, a) => a.indexOf(s) === i);
     const isRegressionModel: boolean = this.type.isRegression;
     const comparisons = Object.keys(Comparison).map(c => {
       return { value: Comparison[c], label: c };
     });
     let controls = [];
 
-    for (const variable of variables) {
-      const existingVariable: Variable = existingVariables.find(v => v.name === variable.name && v.source === variable.source);
-
-      variable.roles = this.getApplicableVariableRoles(existingVariables, variable);
-      variable.role = existingVariable && variable.roles.includes(existingVariable.role) ? existingVariable.role : null;
-      variable.comparison = existingVariable ? existingVariable.comparison : null;
-      variable.value = existingVariable ? existingVariable.value : null;
-      variable.key = null;
-      variable.key_required = false;
-      variable.hint = null;
-
-      if (existingVariable && existingVariable.key && variable.role) {
-        variable.key = existingVariable.key;
-        variable.value_type = variable.parameters.find(p => p.key === variable.key).value_type;
-      }
-
-      if (variable.role === Role.FEATURE && variable.source === Source.GOOGLE_ANALYTICS) {
-        variable.comparisons = comparisons;
-        variable.key_required = false;
-        if (!variable.key) {
-          variable.comparison = null;
-          variable.value = null;
+    if (existingVariables.length) {
+      for (let existingVariable of existingVariables) {
+        const matchingVariables = variables.filter(v => v.source === existingVariable.source);
+        if (!matchingVariables.length) {
+          continue;
         }
+
+        let variable = matchingVariables.find(v => v.name === existingVariable.name);
+        if (!variable) {
+          controls.push(this._fb.group({
+            sources: [variableSources],
+            source: [existingVariable.source],
+            names: [variables.filter(v => v.source === existingVariable.source).map(v => v.name)],
+            name: [null],
+          }));
+          continue;
+        }
+
+        variable.roles = this.getApplicableVariableRoles(existingVariables, variable);
+        variable.role = existingVariable && variable.roles.includes(existingVariable.role) ? existingVariable.role : null;
+        variable.comparison = existingVariable ? existingVariable.comparison : null;
+        variable.value = existingVariable ? existingVariable.value : null;
+        variable.key = null;
+        variable.key_required = false;
+        variable.hint = null;
+
+        if (existingVariable && existingVariable.key && variable.role) {
+          variable.key = existingVariable.key;
+          variable.value_type = variable.parameters.find(p => p.key === variable.key).value_type;
+        }
+
+        if (variable.role === Role.FEATURE && variable.source === Source.GOOGLE_ANALYTICS) {
+          variable.comparisons = comparisons;
+          variable.key_required = false;
+          if (!variable.key) {
+            variable.comparison = null;
+            variable.value = null;
+          }
+        }
+
+        if (variable.role === Role.LABEL && variable.source === Source.GOOGLE_ANALYTICS) {
+          variable.key_required = true;
+          variable.hint = `${isRegressionModel ? 'First value' : 'Trigger event'} will be automatically derived from the first occurrence of this event if not assigned.`;
+        }
+
+        if ([Role.FIRST_VALUE, Role.TRIGGER_EVENT].includes(variable.role) && variable.source === Source.GOOGLE_ANALYTICS) {
+          variable.key_required = true;
+          variable.hint = 'Trigger date will be automatically derived from the first date associated with this event.';
+        }
+
+        if (variable.role === Role.GCLID) {
+          variable.hint = 'Trigger date field will be used as the datetime for the conversion sent to Google Ads.';
+        }
+
+        const control = this._fb.group({
+          sources: [variableSources],
+          source: [variable.source, this.enumValidator(Source)],
+          names: [variables.filter(v => v.source === existingVariable.source).map(v => v.name)],
+          name: [variable.name],
+          placeholder: 'Key' + (variable.key_required ? ' *' : ''),
+          count: [variable.count],
+          roles: [variable.roles],
+          role: [variable.role, this.enumValidator(Role)],
+          parameters: [variable.key_required || variable.comparisons ? variable.parameters : null],
+          key: [
+            variable.key,
+            variable.key_required ? [Validators.required] : []
+          ],
+          comparisons: [variable.comparisons],
+          comparison: [variable.comparison, this.enumValidator(Comparison)],
+          value: [variable.value],
+          value_type: [variable.value_type],
+          hint: variable.hint
+        });
+
+        control.setValidators(this.variableValidator(existingVariables));
+        controls.push(control);
       }
-
-      if (variable.role === Role.LABEL && variable.source === Source.GOOGLE_ANALYTICS) {
-        variable.key_required = true;
-        variable.hint = `${isRegressionModel ? 'First value' : 'Trigger event'} will be automatically derived from the first occurrence of this event if not assigned.`;
-      }
-
-      if ([Role.FIRST_VALUE, Role.TRIGGER_EVENT].includes(variable.role) && variable.source === Source.GOOGLE_ANALYTICS) {
-        variable.key_required = true;
-        variable.hint = 'Trigger date will be automatically derived from the first date associated with this event.';
-      }
-
-      if (variable.role === Role.GCLID) {
-        variable.hint = 'Trigger date field will be used as the datetime for the conversion sent to Google Ads.';
-      }
-
-      const control = this._fb.group({
-        name: [variable.name],
-        placeholder: 'Key' + (variable.key_required ? ' *' : ''),
-        source: [variable.source, this.enumValidator(Source)],
-        count: [variable.count],
-        roles: [variable.roles],
-        role: [variable.role, this.enumValidator(Role)],
-        parameters: [variable.key_required || variable.comparisons ? variable.parameters : null],
-        key: [
-          variable.key,
-          variable.key_required ? [Validators.required] : []
-        ],
-        comparisons: [variable.comparisons],
-        comparison: [variable.comparison, this.enumValidator(Comparison)],
-        value: [variable.value],
-        value_type: [variable.value_type],
-        hint: variable.hint
-      });
-
-      control.setValidators(this.variableValidator(existingVariables));
-      controls.push(control);
+    } else {
+      controls.push(this._fb.group({
+        sources: [variableSources],
+        source: [null]
+      }));
     }
 
     this.mlModelForm.setControl('variables', this._fb.array(controls));
